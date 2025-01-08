f5f050212eebe3e61eace7ad2769e7736e2487e8
Dhanushvg <dhanushv247@gmail.com>
2025-01-08 11:12:34 +0530

    Page creation and Routing have been completed.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import Home from './pages/home'
+import Profile from './pages/profile'
+import Contact from './pages/Contact'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import About from './pages/About'
 
 export default function App() {
   return (
-    <div>App</div>
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/profile' element={<Profile/>}/>
+      <Route path='/contact' element={<Contact/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='sign-in' element={<SignIn/>}/>
+      <Route path='sign-up' element={<SignUp/>}/>
+    </Routes>
+    </BrowserRouter>
   )
 }
