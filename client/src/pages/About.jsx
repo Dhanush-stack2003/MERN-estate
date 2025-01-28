715eaad5cb27007d745bc46775d76d4cbc6b5051
Dhanushvg <dhanushv247@gmail.com>
2025-01-28 17:16:41 +0530

    create about page UI

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,45 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
-  )
+    <div className='lg:max-w-6xl mx-auto my-10 p-10'>
+      <h1 className='text-3xl text-slate-900 font-semibold text-center mb-5'>About AuraEstate</h1>
+      <p className='flex flex-col text-slate-600 mb-8'>
+        <span className='text-slate-700 font-bold'>Who We Are</span>
+      <hr className='h-5 w-20 text-slate-500'/>
+        Welcome to Aura Estate, where we bring your property dreams to life.
+        With a legacy of excellence in the real estate market, we are dedicated
+        to helping clients find their ideal homes, offices, or investment
+        properties. Our passionate team of experts is committed to providing a
+        personalized experience that combines professionalism, trust, and
+        transparency. At Aura Estate, we believe every property holds potential
+        and promise, and we are here to guide you every step of the way toward
+        making the perfect choice.
+      </p>
+      <p className='flex flex-col text-slate-600 mb-8'>
+        <span className='text-slate-700 font-bold'>Our Mission</span>
+      <hr className='h-5 w-20 text-slate-500'/>
+        At Aura Estate, our mission is to transform the real estate experience
+        into one of ease, excitement, and satisfaction. We aim to empower our
+        clients by providing them with the best market insights, innovative
+        solutions, and a seamless process from start to finish. Whether you’re
+        buying, selling, or leasing, our goal is to deliver exceptional value
+        and unmatched service. With a deep understanding of the real estate
+        market and a client-first approach, we strive to help you make
+        confident, informed decisions.
+      </p>
+      <p className='flex flex-col text-slate-600'>
+        <span className='text-slate-700 font-bold'>Why Choose Us</span> 
+        <hr className='h-5 w-20 text-slate-500' />
+        Choosing Aura Estate means choosing a partner who truly
+        understands your vision and works tirelessly to bring it to life. We
+        pride ourselves on our extensive property network, modern tools, and
+        expert knowledge to help you find the perfect property for your needs.
+        From premium residential spaces to commercial investments, our portfolio
+        offers something for everyone. Our commitment to integrity, attention to
+        detail, and dedication to client satisfaction ensures you’ll have a
+        stress-free and rewarding real estate journey. With Aura Estate, your
+        dreams are in trusted hands.
+      </p>
+    </div>
+  );
 }
